e1856427c4d9847477e3684bf2641062d9dd6c9e
DavideDaniel <David@Davids-MacBook-Pro.local>
2014-12-19 13:59:52 -0500

    css changes

--- server.js
+++ server.js
@@ -93,13 +93,13 @@ var server = http.createServer( function ( request, response )
     var index = data1.toString()
     var books = [];
     var i = 0;
-    var Book = function ( name, filename, url_name, author, image_link )
+    var Book = function ( name, filename, url_name, author, image )
     {
       this.name = name;
       this.filename = filename;
       this.url_name = url_name;
       this.author = author;
-      this.image = image_link;
+      this.image = image;
       this.createHTML = function ( index ) {
         fs.readFile(filename, function ( err, data2 ) {
 
@@ -119,7 +119,7 @@ var server = http.createServer( function ( request, response )
     }
     var Life_of_Chopin = new Book( "Life of Chopin", "Life_of_Chopin.txt", "/Life_of_Chopin", "Franz Liszt", "http://ecx.images-amazon.com/images/I/51-pei3JoUL._SY344_BO1,204,203,200_.jpg" );
     books.push(Life_of_Chopin );
-    var Frankenstein = new Book("Frankenstein", "frankenstein.txt","/Frankenstein","Mary Wollstonecraft Shelley", "http://www.cardiffbooktalk.co.uk/sites/default/files/imagecache/cu_book_cover_large/book/cover/Frank...");
+    var Frankenstein = new Book("Frankenstein", "frankenstein.txt","/Frankenstein","Mary Wollstonecraft Shelley", "http://www.cardiffbooktalk.co.uk/sites/default/files/imagecache/cu_book_cover_large/book/cover/Frankenstein_large.jpg");
     books.push(Frankenstein);
     var Sherlock_Holmes = new Book("Sherlock Holmes", "sherlock_holmes.txt", "/Sherlock_Holmes", "Arthur Conan Doyle", "http://img1.imagesbn.com/p/9780062085740_p0_v1_s260x420.JPG");
     books.push(Sherlock_Holmes);
@@ -130,13 +130,18 @@ var server = http.createServer( function ( request, response )
     var Alice_in_Wonderland = new Book("Alice's Adventures in Wonderland", "Alice_Adventures.txt","/Alice_in_Wonderland","Lewis Carroll","http://www.blossombooks.co.uk/shop_image/product/11085.jpg" );
     books.push(Alice_in_Wonderland);
 
+    
+    
+
     for (var i = 0; i < books.length; i++)
         {
+    
+
     if ( path === "/" || path === "/favicon.ico" )
       {
-        fs.readFile("index.html", function ( err, data2 )
+          fs.readFile("index.html", function ( err, data3 )
         {
-          response.end(data2)
+          response.end(data3)
         })
       }
 
